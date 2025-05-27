287be6142239ee907d869e5f7b831ae4e0205e82
Keven-lopez <klopezp40@miumg.edu.gt>
2025-05-27 16:44:21 -0600

    Project Initialization Connection to MySql db configured Connected with RabbitMQ

---
+++ src/main/java/org/prograIII/ProyectoFinalPrograIII/ProyectoFinalPrograIiiApplication.java
@@ -0,0 +1,26 @@
+package org.prograIII.ProyectoFinalPrograIII;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProyectoFinalPrograIiiApplication implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Recv receiver;
+
+	public ProyectoFinalPrograIiiApplication(Recv receiver, RabbitTemplate rabbitTemplate) {
+		this.receiver = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(Send.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+	}
+}
